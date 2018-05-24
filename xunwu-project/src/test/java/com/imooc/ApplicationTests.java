1b608d65d31c4549c06ff8fccc2d37f27b3ed713
peter <610002997@qq.com>
2018-05-24 21:42:37 +0800

    3-3 集成单元测试以及配置内存db

--- xunwu-project/src/test/java/com/imooc/ApplicationTests.java
+++ xunwu-project/src/test/java/com/imooc/ApplicationTests.java
@@ -3,14 +3,14 @@ package com.imooc;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class XunwuProjectApplicationTests {
-
-	@Test
-	public void contextLoads() {
-	}
+@Configuration
+@ActiveProfiles("test")
+public class ApplicationTests {
 
 }
