f207c62c211bc357e56c40df2a34ed707f5ffd48
peter <610002997@qq.com>
2018-05-26 13:06:12 +0800

    5-1 添加后台管理页面

---
+++ xunwu-project/src/main/java/com/imooc/web/admin/AdminController.java
@@ -0,0 +1,50 @@
+package com.imooc.web.admin;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ * Created by 瓦力.
+ */
+@Controller
+public class AdminController {
+
+    /**
+     * 后台管理中心
+     * @return
+     */
+    @GetMapping("/admin/center")
+    public String adminCenterPage() {
+        return "admin/center";
+    }
+
+    /**
+     * 欢迎页
+     * @return
+     */
+    @GetMapping("/admin/welcome")
+    public String welcomePage() {
+        return "admin/welcome";
+    }
+
+    /**
+     * 管理员登录页
+     * @return
+     */
+    @GetMapping("/admin/login")
+    public String adminLoginPage() {
+        return "admin/login";
+    }
+
+    /**
+     * 房源列表页
+     * @return
+     */
+    @GetMapping("admin/house/list")
+    public String houseListPage() {
+        return "admin/house-list";
+    }
+
+//
+}
+
