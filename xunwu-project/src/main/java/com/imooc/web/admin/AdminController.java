b2efc434cf2087118ee093f520d6184f1afa9351
peter <610002997@qq.com>
2018-05-29 23:00:25 +0800

    6-1 添加七牛云图片上传功能

--- xunwu-project/src/main/java/com/imooc/web/admin/AdminController.java
+++ xunwu-project/src/main/java/com/imooc/web/admin/AdminController.java
@@ -1,13 +1,34 @@
 package com.imooc.web.admin;
 
+import com.google.gson.Gson;
+import com.imooc.base.ApiResponse;
+import com.imooc.service.house.IQiNiuService;
+import com.imooc.web.dto.QiNiuPutRet;
+import com.qiniu.common.QiniuException;
+import com.qiniu.http.Response;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.io.InputStream;
 
 /**
  * Created by 瓦力.
  */
 @Controller
 public class AdminController {
+
+    @Autowired
+    private IQiNiuService qiNiuService;
+
+    @Autowired
+    private Gson gson;
 
     /**
      * 后台管理中心
@@ -37,14 +58,48 @@ public class AdminController {
     }
 
     /**
-     * 房源列表页
+     * 新增房源功能页
      * @return
      */
-    @GetMapping("admin/house/list")
-    public String houseListPage() {
-        return "admin/house-list";
+    @GetMapping("admin/add/house")
+    public String addHousePage() {
+        return "admin/house-add";
     }
 
-//
+    /**
+     * 上传图片接口
+     * @param file
+     * @return
+     */
+    @PostMapping(value = "admin/upload/photo", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    @ResponseBody
+    public ApiResponse uploadPhoto(@RequestParam("file") MultipartFile file) {
+        if (file.isEmpty()) {
+            return ApiResponse.ofStatus(ApiResponse.Status.NOT_VALID_PARAM);
+        }
+
+        try {
+            InputStream inputStream = file.getInputStream();
+            Response response = qiNiuService.uploadFile(inputStream);
+            if (response.isOK()) {
+                QiNiuPutRet ret = gson.fromJson(response.bodyString(), QiNiuPutRet.class);
+                return ApiResponse.ofSuccess(ret);
+            } else {
+                return ApiResponse.ofMessage(response.statusCode, response.getInfo());
+            }
+
+        } catch (QiniuException e) {
+            Response response = e.response;
+            try {
+                return ApiResponse.ofMessage(response.statusCode, response.bodyString());
+            } catch (QiniuException e1) {
+                e1.printStackTrace();
+                return ApiResponse.ofStatus(ApiResponse.Status.INTERNAL_SERVER_ERROR);
+            }
+        } catch (IOException e) {
+            return ApiResponse.ofStatus(ApiResponse.Status.INTERNAL_SERVER_ERROR);
+        }
+    }
+
 }
 
